5f637b4e5798a140978c2765d77c674207949e44
Mattia Barbon <mattia@barbon.org>
2011-10-29 15:37:40 +0200

    Added stub Scala Android application.

---
+++ src/MyFilms.scala
@@ -0,0 +1,12 @@
+package org.barbon.myfilms;
+
+import _root_.android.app.Activity;
+
+import _root_.android.os.Bundle;
+
+class MyFilms extends Activity {
+    override def onCreate(savedInstanceState : Bundle) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
