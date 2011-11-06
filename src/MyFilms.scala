bae407ed1fe467133a646824cc1f7bcca6f2ed01
Mattia Barbon <mattia@barbon.org>
2011-11-06 17:54:57 +0100

    Fixed method visibility to match superclass.

--- src/MyFilms.scala
+++ src/MyFilms.scala
@@ -5,7 +5,7 @@ import _root_.android.app.Activity;
 import _root_.android.os.Bundle;
 
 class MyFilms extends Activity {
-    override def onCreate(savedInstanceState : Bundle) {
+    protected override def onCreate(savedInstanceState : Bundle) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
